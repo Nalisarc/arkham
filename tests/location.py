d5a7d0f1281f4fbb212a72f6fba5e40ef6954afd
Daniel Alexander Smith <nalisarc@gmail.com>
2017-07-09 15:30:13 -0600

    Finished Connections on Arkham Table
    I wrote some documentation for the arkham table.
    I finished transcribing out the loctions in arkham, and made a pretty
    table from their data.
    Next I'll work on passing this information to python.

--- tests/location.py
+++ tests/location.py
@@ -3,8 +3,13 @@ import unittest
 
 class Location(object):
 
-    def __init__(self):
-        pass
+    def __init__(self,
+                 name=None,
+                 connections=(),
+    ):
+
+        self.name = name
+        self.connections = connections
 
 
 
@@ -16,3 +21,16 @@ class LocationTests(unittest.TestCase):
             test_location
         except:
             self.fail("Test location failed to be called")
+
+
+    def test_can_set_location_attributes(self):
+        """A test to set various attributes of locations"""
+        test_location = Location(name="Test")
+
+        self.assertEqual(test_location.name, "Test")
+
+    def test_locations_have_connections(self):
+        """Test to make sure that locations can be passes a tuple of connections"""
+        test_location = Location(connections=("t1","t2"))
+
+        self.assertEqual(test_location.connections, ("t1","t2"))
